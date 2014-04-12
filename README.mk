e80d8c45acae5042347b494da4af088c6e63aa76
marklin2100 <marklin2100@gmail.com>
2014-04-12 13:50:26 +0800

    add README.mk file

---
+++ README.mk
@@ -0,0 +1,22 @@
+Lynx innovation projects
+
+Quick setup — if you've done this kind of thing before
+or
+HTTP SSH
+
+We recommend every repository include a README, LICENSE, and .gitignore.
+Create a new repository on the command line
+
+touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/marklin2100/Projects.git
+git push -u origin master
+
+Push an existing repository from the command line
+
+git remote add origin https://github.com/marklin2100/Projects.git
+git push -u origin master
+
+
